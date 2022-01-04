e1e9b8a06148b6da3b4cbbdc4e2bd0fdb3a1a948
meharsh2001 <94461844+meharsh2001@users.noreply.github.com>
2022-01-04 19:10:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
-const port =process.env.PORT || 8000;
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
@@ -39,4 +38,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(port, "127.0.0.1");
+server.listen(process.env.PORT || 8080, "127.0.0.1");
